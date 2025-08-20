b01a983896fb38a6cab0b1a670f2ad1d289b2473
Eshiwani Michael <michaelndolo17142@gmail.com>
2025-08-20 14:15:57 +0300

    sha256 file working

--- js/sha256.js
+++ js/sha256.js
@@ -1,6 +1,12 @@
-async function sha256(message) {
+async function sha256FromText(message) {
   const msgBuffer = new TextEncoder().encode(message);
   const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+}
+
+async function sha256FromBuffer(arrayBuffer) {
+  const hashBuffer = await crypto.subtle.digest("SHA-256", arrayBuffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
 }
@@ -11,22 +17,22 @@ async function generateHash() {
     document.getElementById("hashResult").innerText = "Please enter some text.";
     return;
   }
-  const hash = await sha256(text);
+  const hash = await sha256FromText(text);
   document.getElementById("hashResult").innerText = hash;
 }
 
 async function generateFileHash() {
-      const fileInput = document.getElementById("inputFile");
-      if (fileInput.files.length === 0) {
-        document.getElementById("hashResult").innerText = "Please select a file.";
-        return;
-      }
+  const fileInput = document.getElementById("inputFile");
+  if (fileInput.files.length === 0) {
+    document.getElementById("hashResult").innerText = "Please select a file.";
+    return;
+  }
 
-      const file = fileInput.files[0];
-      const arrayBuffer = await file.arrayBuffer();
-      const hash = await sha256(arrayBuffer);
-      document.getElementById("hashResult").innerText = hash;
-    }
+  const file = fileInput.files[0];
+  const arrayBuffer = await file.arrayBuffer();
+  const hash = await sha256FromBuffer(arrayBuffer);
+  document.getElementById("hashResult").innerText = hash;
+}
 
 function copyHash() {
   const hash = document.getElementById("hashResult").innerText;
